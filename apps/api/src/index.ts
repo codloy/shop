7ff53da3cabc974ba88e1c1446a7475f4ea0aa36
toumku <toumku@gmail.com>
2024-08-06 20:02:05 +0800

    port console log added

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -11,7 +11,8 @@ listen(port, ip);
 
 console.log('API:');
 console.log(`PORT="${port}"`);
-console.log(`DB_URL="${process.env.DB_URL}"\n`);
+console.log(`DB_URL="${process.env.DB_URL}"`);
+console.log(`ACCESS_TOKEN_SECRET="${process.env.ACCESS_TOKEN_SECRET}"\n`);
 console.log('WEB:');
 console.log(`NEXT_PUBLIC_HTTP_URL="http://${ip}:${port}"`);
 console.log(`NEXT_PUBLIC_WSS_URL="ws://${ip}:${port}"`);
